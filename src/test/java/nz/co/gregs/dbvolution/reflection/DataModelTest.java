f88112ca3559be7abc418e63d8c81f16f34d260d
Gregory Graham <gregory@gregs.co.nz>
2025-08-25 13:38:07 +1200

    Added test to hopefully help test how well everything works when the system is working hard

--- src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
+++ src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
@@ -482,6 +482,7 @@ public class DataModelTest extends AbstractTest {
 		knownKeys.add("class nz.co.gregs.dbvolution.DBMigrationTest$MigrateHeroesAndSelectedVilliansToFight");
 		knownKeys.add("class nz.co.gregs.dbvolution.actions.DBInsertTest$testDefaultValuesAreConsistentInCluster");
 		knownKeys.add("class nz.co.gregs.dbvolution.databases.metadata.MySQLDBDatabaseMetaData$STGeometryColumns");
+    knownKeys.add("class nz.co.gregs.dbvolution.datatypes.DBEncryptedTextTest$EncryptedTextTestTableWithThreads");
 
 		// Store the number found for use later
 		final int numberOfClassesFound = foundKeys.size();
@@ -656,6 +657,7 @@ public class DataModelTest extends AbstractTest {
 		knownKeys.add("nz.co.gregs.dbvolution.DBDatabaseTest.RequiredTableShouldBeCreatedAutomatically");
 		knownKeys.add("nz.co.gregs.dbvolution.actions.DBInsertTest.testDefaultValuesAreConsistentInCluster");
 		knownKeys.add("nz.co.gregs.dbvolution.databases.metadata.MySQLDBDatabaseMetaData.STGeometryColumns");
+    knownKeys.add("nz.co.gregs.dbvolution.datatypes.DBEncryptedTextTest.EncryptedTextTestTableWithThreads");
 
 		// Store the number found for use later
 		final int numberOfFoundClasses = foundClasses.size();
