9a0ee7298e5b457210c6dbd80621f8a9c98e7eb5
Gregory Graham <gregory@gregs.co.nz>
2026-03-20 12:28:40 +1300

    Moved logging of exceptions to a method to add a bit of consistency

---
+++ src/main/java/nz/co/gregs/dbvolution/internal/database/LoggedException.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2026 Gregory Graham.
+ *
+ * Commercial licenses are available, please contact info@gregs.co.nz for details.
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ 
+ * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * 
+ * You are free to:
+ *     Share - copy and redistribute the material in any medium or format
+ *     Adapt - remix, transform, and build upon the material
+ * 
+ *     The licensor cannot revoke these freedoms as long as you follow the license terms.               
+ *     Under the following terms:
+ *                 
+ *         Attribution - 
+ *             You must give appropriate credit, provide a link to the license, and indicate if changes were made. 
+ *             You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+ *         NonCommercial - 
+ *             You may not use the material for commercial purposes.
+ *         ShareAlike - 
+ *             If you remix, transform, or build upon the material, 
+ *             you must distribute your contributions under the same license as the original.
+ *         No additional restrictions - 
+ *             You may not apply legal terms or technological measures that legally restrict others from doing anything the 
+ *             license permits.
+ * 
+ * Check the Creative Commons website for any details, legalese, and updates.
+ */
+package nz.co.gregs.dbvolution.internal.database;
+
+import java.util.logging.Level;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class LoggedException {
+
+  private final Throwable exception;
+  private final Level level;
+  private final String message;
+  private final Object[] messageItems;
+
+  public LoggedException(Throwable exception, Level level, String message, Object[] messageItems) {
+    this.exception = exception;
+    this.level = level;
+    this.message = message;
+    this.messageItems = messageItems;
+  }
+
+  /**
+   * @return the exception
+   */
+  public Throwable getException() {
+    return exception;
+  }
+
+  /**
+   * @return the level
+   */
+  public Level getLevel() {
+    return level;
+  }
+
+  /**
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
+  }
+
+  /**
+   * @return the messageItems
+   */
+  public Object[] getMessageItems() {
+    return messageItems;
+  }
+  
+}
